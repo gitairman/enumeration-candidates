4eecc75388cf388ffae35aad99329be6845c9086
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-28 13:12:08 -0700

    added filter methods

--- filters.rb
+++ filters.rb
@@ -1,16 +1,41 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
+require './candidates'
 
 def find(id)
-    # Your code Here
+  @candidates.select { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.select { |candidate| 
+    experienced?(candidate) && 
+    github_points_over_100?(candidate) &&
+    knows_ruby_or_python?(candidate) && 
+    applied_in_last_15_days?(candidate) &&
+    over_17?(candidate) }
   end
   
   # More methods will go below
+  def github_points_over_100?(candidate)
+    candidate[:github_points] >= 100
+  end
+
+  def knows_ruby_or_python?(candidate)
+    candidate[:languages].include?('Ruby' || 'Python')
+  end
+
+  def applied_in_last_15_days?(candidate)
+    DateTime.now - candidate[:date_applied] < 15
+  end
+
+  def over_17?(candidate)
+    candidate[:age] > 17
+  end
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
--- main.rb
+++ main.rb
@@ -6,4 +6,12 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# pp @candidates
+
+# pp github_points_over_100?(@candidates[0]) # true
+# pp experienced?(@candidates[1]) # false
+# pp qualified_candidates(@candidates)
+# pp applied_in_last_15_days?(@candidates[0]) # true
+# pp applied_in_last_15_days?(@candidates[1]) # false
+# pp find(7)
+pp ordered_by_qualifications(@candidates)
